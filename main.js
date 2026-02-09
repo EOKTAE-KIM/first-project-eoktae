022fa3144d77d4588d261d3d08470c79d0f7e370
kimkt1002 <kimkt1002@gmail.com>
2026-02-09 14:15:59 +0000

    Upload all current project files

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// Add JS here
+const lottoNumbersDiv = document.getElementById("lotto-numbers");
+const generateButton = document.getElementById("generate-button");
+
+const generateLottoNumbers = () => {
+  lottoNumbersDiv.innerHTML = "";
+  const numbers = new Set();
+  while (numbers.size < 6) {
+    numbers.add(Math.floor(Math.random() * 45) + 1);
+  }
+
+  for (const number of [...numbers].sort((a, b) => a - b)) {
+    const numberDiv = document.createElement("div");
+    numberDiv.classList.add("lotto-number");
+    numberDiv.textContent = number;
+    lottoNumbersDiv.appendChild(numberDiv);
+  }
+};
+
+generateButton.addEventListener("click", generateLottoNumbers);
+
+generateLottoNumbers();
